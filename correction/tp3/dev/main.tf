0686a2600a783f2d34c956810a7f99f34cf3737e
Julien VIallon <julien.viallon@michelin.com>
2026-06-03 13:28:57 +0200

    Review TP5

--- correction/tp3/dev/main.tf
+++ correction/tp3/dev/main.tf
@@ -6,11 +6,5 @@ resource "azurerm_resource_group" "dev" {
 resource "azurerm_network_security_group" "nsg_dev" {
   name                = "nsg-tp3-dev"
   location            = azurerm_resource_group.dev.location
-  resource_group_name = azurerm_resource_group.dev.name
+  resource_group_name = azurerm_resource_group.dev.resource_group_name
 }
-
-#resource "azurerm_network_security_rule" "dev" {
-#  name                        = "dev"
-#  resource_group_name         = azurerm_resource_group.dev.name
-#  network_security_group_name = azurerm_network_security_group.nsg_dev.name
-#}
